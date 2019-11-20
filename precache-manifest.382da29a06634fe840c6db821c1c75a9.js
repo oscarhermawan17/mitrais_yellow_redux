a7ee93ed5cf86de59a2634f8643516cf3a88414f
Oscar Hermawan <Oscar_H261@mitrais.com>
2019-11-20 16:58:42 +0800

    Updates

---
+++ precache-manifest.382da29a06634fe840c6db821c1c75a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b635902ad86656307fa17b2e46d90af",
+    "url": "/mitrais_yellow_redux/index.html"
+  },
+  {
+    "revision": "8c9f0f9e9b651facd941",
+    "url": "/mitrais_yellow_redux/static/css/main.46ddce37.chunk.css"
+  },
+  {
+    "revision": "54d4e60a9b5f520f4222",
+    "url": "/mitrais_yellow_redux/static/js/2.6b9ab53c.chunk.js"
+  },
+  {
+    "revision": "8c9f0f9e9b651facd941",
+    "url": "/mitrais_yellow_redux/static/js/main.e560abd9.chunk.js"
+  },
+  {
+    "revision": "e8238b5fd43c225251f3",
+    "url": "/mitrais_yellow_redux/static/js/runtime-main.f73ef7bb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mitrais_yellow_redux/precache-manifest.8f566cb720394db6537b120f41ed8177.js"
+  "/mitrais_yellow_redux/precache-manifest.382da29a06634fe840c6db821c1c75a9.js"
 );
 
 self.addEventListener('message', (event) => {
